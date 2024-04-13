f8e1c944f9915b9b6e6bb866fb55fa4af703a498
Mohamed Gad <mohamed.gad.edu@gmail.com>
2024-04-13 16:59:33 +0200

    Ready for merge

--- src/Main.java
+++ src/Main.java
@@ -5,42 +5,15 @@ public class Main extends PApplet {
         PApplet.main("Main");
     }
 
-    int canvasHeight = 500;
-    int canvasWidth = 500;
-
-    public void settings() {
-        size(canvasWidth, canvasHeight);
-    }
-
-    int balloonNum = 2;
-
-    Balloon balloons[] = new Balloon[balloonNum];
-
-    public void setup() {
-        int x = 0;
-        int dx = Balloon.width;
-        for (int i = 0; i < balloonNum; i++) {
-            balloons[i] = new Balloon(this, x, 'r');
-            balloons[i].setup();
-            x += (dx+10);
-        }
-    }
-
-    public void mousePressed() {
-        for (Balloon b : balloons) {
-            b.pop();
-        }
+    public void settings(){
 
     }
 
+    public void setup(){
 
-    public void draw() {
-        background(155);
+    }
 
-        for (Balloon b : balloons) {
-            b.showWithBoundaryBox();
-            b.updateYPos();
-        }
+    public void draw(){
 
     }
 }
